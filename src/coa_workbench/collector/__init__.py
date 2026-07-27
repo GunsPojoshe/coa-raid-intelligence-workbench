7c0bc4adc24a4f5174b75cfc908d588de992be72
GunsPojoshe <simpataga17@gmail.com>
2026-07-27 10:38:14 +0300

    feat: add collector source registry package

---
+++ src/coa_workbench/collector/__init__.py
@@ -0,0 +1,13 @@
+from .source_registry import (
+    SourceRegistry,
+    SourceRoute,
+    UnverifiedSourceRouteError,
+    load_source_registry,
+)
+
+__all__ = [
+    "SourceRegistry",
+    "SourceRoute",
+    "UnverifiedSourceRouteError",
+    "load_source_registry",
+]
