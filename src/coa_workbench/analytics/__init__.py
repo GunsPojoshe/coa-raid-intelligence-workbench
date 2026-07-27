932f76899a13be8431c277b6bd80baf493e85bd4
GunsPojoshe <simpataga17@gmail.com>
2026-07-27 10:35:07 +0300

    feat: add analytics trust package

---
+++ src/coa_workbench/analytics/__init__.py
@@ -0,0 +1,13 @@
+from .trust import (
+    CANONICAL_TRUST_STATUSES,
+    DataTrustStatus,
+    is_canonical,
+    require_canonical,
+)
+
+__all__ = [
+    "CANONICAL_TRUST_STATUSES",
+    "DataTrustStatus",
+    "is_canonical",
+    "require_canonical",
+]
