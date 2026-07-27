c206a3d9c12d1dd4f24624b37fa84d6dccdb9f35
GunsPojoshe <simpataga17@gmail.com>
2026-07-27 10:47:23 +0300

    refactor: expose collector ingestion primitives

--- src/coa_workbench/collector/__init__.py
+++ src/coa_workbench/collector/__init__.py
@@ -1,3 +1,12 @@
+from .probe import ProbeResult, probe_registry_route, probe_result_to_dict
+from .raw_archive import (
+    RawArchive,
+    RawCapture,
+    capture_to_dict,
+    request_key_from_url,
+    sanitize_url,
+    schema_fingerprint,
+)
 from .source_registry import (
     SourceRegistry,
     SourceRoute,
@@ -6,8 +15,17 @@ from .source_registry import (
 )
 
 __all__ = [
+    "ProbeResult",
+    "RawArchive",
+    "RawCapture",
     "SourceRegistry",
     "SourceRoute",
     "UnverifiedSourceRouteError",
+    "capture_to_dict",
     "load_source_registry",
+    "probe_registry_route",
+    "probe_result_to_dict",
+    "request_key_from_url",
+    "sanitize_url",
+    "schema_fingerprint",
 ]
