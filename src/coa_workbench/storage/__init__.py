a95f363daccb87b1880b36c6c3e6f199bee7bb87
GunsPojoshe <simpataga17@gmail.com>
2026-07-30 01:49:55 +0300

    export combatants observation persistence

--- src/coa_workbench/storage/__init__.py
+++ src/coa_workbench/storage/__init__.py
@@ -1,5 +1,6 @@
 """DuckDB storage helpers."""
 
+from .combatants_observations import persist_observed_combatants_info_observations
 from .migrations import DuckDBUnavailableError, apply_migrations
 from .plans import PlanNotFoundError, PlanRepository
 from .report_slice import persist_observed_report_slice
@@ -9,5 +10,6 @@ __all__ = [
     "PlanNotFoundError",
     "PlanRepository",
     "apply_migrations",
+    "persist_observed_combatants_info_observations",
     "persist_observed_report_slice",
 ]
