4b34f64862826855383d71a4fc4d313fffb63cc8
GunsPojoshe <simpataga17@gmail.com>
2026-07-29 19:09:12 +0300

    export report slice persistence

--- src/coa_workbench/storage/__init__.py
+++ src/coa_workbench/storage/__init__.py
@@ -2,10 +2,12 @@
 
 from .migrations import DuckDBUnavailableError, apply_migrations
 from .plans import PlanNotFoundError, PlanRepository
+from .report_slice import persist_observed_report_slice
 
 __all__ = [
     "DuckDBUnavailableError",
     "PlanNotFoundError",
     "PlanRepository",
     "apply_migrations",
+    "persist_observed_report_slice",
 ]
